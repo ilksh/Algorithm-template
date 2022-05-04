0d53f4d78efd93d03af8f0b5149da980d33c84b6
Sang Hyun Kim <104607302+ilksh@users.noreply.github.com>
2022-05-04 16:21:47 +0900

    Add files via upload

---
+++ C++/Stack (Library).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+typedef int element;
+stack <element> s;  // intialize stack
+
+int main()
+{
+    s.push(1);  // s = [1]
+    s.push(2);  // s = [2(top), 1]
+    s.push(5);  // s = [5 (top), 2, 1]
+    s.pop();
+   
+    // printing from the top
+    while(!s.empty())
+    {
+        auto cur = s.top(); s.pop();
+        cout << " "<<cur;
+    }
+
+}
+
+
