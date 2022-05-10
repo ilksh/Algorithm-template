6474590783f8786287aad285b3bbd2376b1f1266
Sang Hyun Kim <104607302+ilksh@users.noreply.github.com>
2022-05-10 15:39:10 +0900

    Update Queue.cpp

--- C++/Queue.cpp
+++ C++/Queue.cpp
@@ -6,20 +6,20 @@ const int MAX_SIZE = 10001;
 typedef int element;
 typedef struct {
     int front;
-    int back;
+    int rear;
     element array[MAX_SIZE];
 } Queue;
 
 void init(Queue *q)
 {
     q->front = -1;
-    q->back = -1;
+    q->rear = -1;
 }
 
 void print(Queue *q)
 {
     cout << "q: [ ";
-    for(int i= q->front+1; i< q->back+1; i++)
+    for(int i= q->front+1; i< q->rear+1; i++)
         cout << q->array[i]<<" ";
     cout<<"]\n";
 }
@@ -27,7 +27,7 @@ void print(Queue *q)
 bool empty(Queue *q)
 {
     // queue is empty
-    if(q->front == q->back)
+    if(q->front == q->rear)
         return true;
     
     return false;
@@ -36,7 +36,7 @@ bool empty(Queue *q)
 bool full(Queue *q)
 {
     // queue is saturated
-    if(q->back == MAX_SIZE -1)
+    if(q->rear == MAX_SIZE -1)
         return true;
     
     return false;
@@ -51,7 +51,7 @@ void enqueue(Queue *q, element item)
     }
     
     q->back += 1;
-    q->array[q->back] = item;
+    q->array[q->rear] = item;
     print(q);
 }
 
