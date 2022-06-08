c9b876e9e7d919609005e6166fb0ac5aec1281d7
Sang Hyun Kim <ksh727301@gmail.com>
2022-06-08 13:20:48 +0900

    DFS

---
+++ Python/DFS/DFS (adjacency matrix).py
@@ -0,0 +1,48 @@
+from collections import  deque
+
+
+# adjacency matrix
+def adj_check(n, m):
+    # node n and m are connected
+    adj[n][m] = True
+    adj[m][n] = True
+
+
+def dfs(cur):
+    visited[cur] = True
+    ans.append(cur)
+
+    for i in range(1, 10):
+        if adj[cur][i] is True and not visited[i]:
+            dfs(i)
+
+
+def ans_print():
+    global ans
+    print("dfs search order")
+    temp = ans.popleft()
+    print(temp, end="")
+
+    while len(ans) > 0:
+        print("->{}".format(ans.popleft()), end="")
+
+
+if __name__ == '__main__':
+    visited = [False for _ in range(15)]
+    adj = [([False] * 10) for _ in range(10)]
+    ans = deque()
+
+    adj_check(1, 2)
+    adj_check(1, 6)
+    adj_check(1, 7)
+    adj_check(1, 9)
+
+    adj_check(2, 3)
+    adj_check(2, 4)
+
+    adj_check(4, 5)
+
+    adj_check(7, 8)
+
+    dfs(1)
+    ans_print()
