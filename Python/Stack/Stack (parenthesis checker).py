a3922d8fee4b2b1670c5d4e5a5b3de830c821c9d
Sang Hyun Kim <ksh727301@gmail.com>
2022-06-22 20:19:28 +0900

    Stack

--- Python/Stack/Stack (parenthesis checker).py
+++ Python/Stack/Stack (parenthesis checker).py
@@ -1,23 +1,25 @@
-s = []
-stack = []
+lyrics = ["Il ((libro dell'amor) mi annoia), (E pesa come l'anima)",
+          "(È pieno (di carezze) al) (cuore), E modi (per) (danzare",
+          "(Ma, Mi piace quando (lo leggi) tu)",
+          "(((E tu di più), (Tu puoi leggermi) il (cielo) (al blu)",
+          ")Il libro dell'amore (suona))"]
 
+for sentence in lyrics:
+    stack = []
 
-for sentence in s:
     flag = False
+
     for elem in sentence:
         if elem == '(':
             stack.append(elem)
 
         elif elem == ')':
-            if not stack:
+            if len(stack) == 0:
                 flag = True
-                print("error")
-                exit(0)
-
             else:
                 stack.pop()
 
-if stack or not flag:
-    print("error")
-else:
-    print("good")
+    if not flag and not stack:
+        print("success")
+    else:
+        print("error")
