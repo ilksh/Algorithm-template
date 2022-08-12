f8d0081f2e1aa57ccce546823f4a85b5a806fa60
Sang Hyun Kim <104607302+ilksh@users.noreply.github.com>
2022-08-12 12:58:56 +0900

    Update Insertion Sort.cpp

--- C++/Sorting/Insertion Sort.cpp
+++ C++/Sorting/Insertion Sort.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n = 10;
+
 void printList(int list[])
 {
-    for(int i=0; i<10; ++i)
+    for(int i=0; i<n; ++i)
         cout << list[i] << " ";
     cout << "\n";
 }
@@ -12,7 +14,7 @@ void insertionSort(int list[])
 {
     int i, j;
     
-    for(i=1; i<10; ++i){
+    for(i=1; i<n; ++i){
         int key = list[i];
         
         for(j=i-1; j>=0; --j)
